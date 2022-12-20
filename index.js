074ed457a5f8c23f7d486e65a75a6d3b6f2dd663
Kiara Johnson <kiarajohnson91@gmail.com>
2022-12-19 21:09:39 -0600

    Simple JavaScript
    Simple Function using JavaScript

---
+++ index.js
@@ -0,0 +1,5 @@
+function printInput (name, height, country){
+    console.log(name + ' ' + height + ' ' + country);
+    alert(name + ' ' + height + ' ' + country);
+}
+printInput('kiara', '67', 'usa');
